03e551899b4a32869652804efd6b9124777db2d5
Gabriel <gabrielgprzy@gmail.com>
2024-07-03 11:48:36 -0300

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ctf-unisinos
+
+chmod +x ./run.sh
+
+./run.sh
+
+az login
+
+az group create --name ctf-unisinos --location eastus
+
+az deployment group create --resource-group ctf-unisinos-rg  --template-file init.bicep --parameters init.params.json
+
+ansible-playbook -i ./ansible_ctfd/hosts ./ansible_ctfd/playbook.yaml
--- run.sh
+++ run.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 
-RESOURCE_GROUP="ctf-unisinos-rg"
-LOCATION="eastus"
-BICEP_FILE="./bicep/init.bicep"
-ADMIN_USERNAME="unisinos"
-ADMIN_PASSWORD=""
+# Atualiza os repositórios
+sudo apt update
 
-sudo apt-get update
-sudo apt-get install -y curl apt-transport-https lsb-release software-properties-common
-sudo apt install azure-cli
+# Instala o Azure CLI
+echo "Instalando Azure CLI..."
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-az bicep install
+# Instala o Bicep
+echo "Instalando Bicep..."
+curl -Lo bicep https://github.com/Azure/bicep/releases/latest/download/bicep-linux-x64
+chmod +x ./bicep
+sudo mv ./bicep /usr/local/bin/bicep
 
-sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y ansible
+# Instala o Ansible
+echo "Instalando Ansible..."
+sudo apt install -y ansible
 
-echo "Por favor, faça login na sua conta Azure"
-az login
-
-az group create --name $RESOURCE_GROUP --location $LOCATION
-
-az deployment group create --resource-group $RESOURCE_GROUP --template-file $BICEP_FILE --parameters ./bicep/init.params.json
-
-VM_IP=$(az network public-ip show --resource-group $RESOURCE_GROUP --name ${INFRA_PREFIX}-pip --query "ipAddress" --output tsv)
-
-echo -e "[ctfd_server]\n$VM_IP ansible_user=$ADMIN_USERNAME ansible_password=$ADMIN_PASSWORD" > hosts
-
-ansible-playbook -i hosts ./ansible_ctfd/playbook.yaml
-
-echo "Provisionamento concluído, arquivo de hosts do Ansible atualizado e playbook executado."
-
+echo "Instalação concluída."
